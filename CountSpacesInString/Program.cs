1c57f0b1e3aa28ec3cbc83c5378241a83b3de2e2
FinT08 <IppolitovIS@gmail.com>
2023-01-15 14:26:25 +0300

    Добавьте файлы проекта.

---
+++ CountSpacesInString/Program.cs
@@ -0,0 +1,9 @@
+﻿using Extensions.ClassString;
+
+Console.WriteLine("=======Программа подсчёта пробелов в строке=======");
+Console.WriteLine("Введите строку: ");
+string? tempString = Console.ReadLine();
+if (tempString == string.Empty)
+    Console.WriteLine("Вы ввели пустую строку! (пробелов нет)");
+else
+    Console.WriteLine($"Количество пробелов в строке: {tempString.CountSpaces()}");
---
+++ Extensions.ClassString/StrExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Extensions.ClassString
+{
+    public static class StringExtensions
+    {
+        public static int CountSpaces(this string s)
+        {
+            return s.Count(chr => chr == ' ');
+        }
+    }
+}
